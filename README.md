25f40372ddbac0d9a28c0f35503b08ff73fb4675
Ashna Shah <ashna.shah@vista.com>
2023-04-25 18:23:09 +0530

    chore: inter service communication init

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # product-catalog
+
+## steps
+
+- Replace USERMANAGEMENT_URL with local IP
+- `docker build -t productcatalog .`
+- `docker run -p 3000:3000 -it productcatalog`
--- index.js
+++ index.js
@@ -1,16 +1,29 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
+
 const app = express();
 const port = 3000;
+
+// Communication among different services
+const USER_MANAGEMENT_URL = "http://192.168.29.223:5000";
 
 // middleware setup
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // GET route
-app.get('/products', (req, res) => {
-  const products = [{ id: 1, name: 'Business Cards' }, { id: 2, name: 'Post Cards' }];
+app.get("/products", (req, res) => {
+  const products = [
+    { id: 1, name: "Business Cards" },
+    { id: 2, name: "Post Cards" },
+  ];
   res.send(products);
+});
+
+app.get("/health", async (req, res) => {
+  const response = await axios.get(`${USER_MANAGEMENT_URL}/health`);
+  res.json(response.data);
 });
 
 // start server
