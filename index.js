653f7d7afb38cba9e973842aa23051365de130a1
vedant.sakhardande <Vedant.Sakhardande@cimpress.com>
2023-04-27 00:44:56 +0530

    add product catalog routes

--- index.js
+++ index.js
@@ -1,29 +1,91 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const axios = require("axios");
-
+const jwt = require('jsonwebtoken');
+const axios = require("axios")
 const app = express();
-const port = 3000;
+const port = 5002;
 
-// Communication among different services
-const USER_MANAGEMENT_URL = "http://192.168.29.223:5000";
+const secret = "314781839wjd3190u4edn13ed381de31bfu13ii"
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://productmanagement:productmanagement@cluster0.tch1uh5.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
 
 // middleware setup
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // GET route
-app.get("/products", (req, res) => {
-  const products = [
-    { id: 1, name: "Business Cards" },
-    { id: 2, name: "Post Cards" },
-  ];
-  res.send(products);
+app.get("/health", (req, res) => {
+  res.json({ Health: "Okay" });
 });
 
-app.get("/health", async (req, res) => {
-  const response = await axios.get(`${USER_MANAGEMENT_URL}/health`);
-  res.json(response.data);
+app.get("/products", async(req, res) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  if (!token) {
+    return res.sendStatus(401);
+  }
+
+  jwt.verify(token, secret, async (err, decoded) => {
+    if (err) {
+      return res.sendStatus(403);
+    }
+    // If the token is valid, save the user details to the request object
+    req.user = decoded;
+    const db = client.db('product-management');
+    const productsCollection = db.collection('products');
+    const products = await productsCollection.find({}).toArray();
+    res.send(products)
+  });
+  
+});
+
+
+app.post("/order", async(req, res) => {
+   const { items } = req.body
+   await items.forEach(async(item) => {
+    axios.post('http://localhost:5003/inventory', {
+    productName: item.productName,
+    stock: item.stock,
+  })
+  .then(response => {
+    const isValid = response.data
+    if(!isValid){
+      res.send(`Product ${item.productName} is out of stock`)
+    }
+  })
+  .catch(error => {
+    console.error(error);
+  });
+   });
+
+   await items.forEach(async(item) => {
+    axios.patch('http://localhost:5003/inventory', {
+    productName: item.productName,
+    stock: item.stock,
+  })
+  .then(response => {
+    const isValid = response.data
+    if(!isValid){
+      res.send(`Something went wrong`)
+    }
+  })
+  .catch(error => {
+    console.error(error);
+  });
+   });
+   res.send("Order Placed")
 });
 
 // start server
